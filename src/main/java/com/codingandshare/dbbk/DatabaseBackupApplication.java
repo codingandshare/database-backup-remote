90cba9025ccd4fc973b30ed9fba50b9d21f065b6
Nhan Dinh <huunhancit@gmail.com>
2021-04-23 21:59:10 +0700

    Add plugin checkstyle java

--- src/main/java/com/codingandshare/dbbk/DatabaseBackupApplication.java
+++ src/main/java/com/codingandshare/dbbk/DatabaseBackupApplication.java
@@ -3,11 +3,22 @@ package com.codingandshare.dbbk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class to start Service.
+ *
+ * @author Nhan Dinh
+ * @since 4/23/21
+ **/
 @SpringBootApplication
 public class DatabaseBackupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseBackupApplication.class, args);
-	}
+  /**
+   * Main function to start service.
+   *
+   * @param args input parameters from command line.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(DatabaseBackupApplication.class, args);
+  }
 
 }
